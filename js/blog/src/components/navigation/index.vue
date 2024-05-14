2650bcf25e88036307a29061e81419f3602992f0
zhangchenyang <15806629859@139.com>
2024-05-14 21:54:37 +0800

    fix:'commit’

--- js/blog/src/components/navigation/index.vue
+++ js/blog/src/components/navigation/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, defineProps, onMounted, shallowRef } from 'vue';
+import { ref,  onMounted, shallowRef } from 'vue';
 import Home from '@/views/Home/index.vue'
 import Article from '@/views/Article/index.vue'
 import About from '@/views/About/index.vue'
@@ -82,56 +82,4 @@ onMounted(
 }
 
 </style>
-<script setup lang="ts">
-const abc = 'asd'
-</script>
 
-<template>
-  <div class="navigation">
-    <div class="version_heart">
-      <div class="title">首页</div>
-      <div class="title">文章</div>
-      <div class="title">友链</div>
-      <div class="title">关于</div>
-    </div>
-  </div>
-</template>
-
-<style lang="scss" scoped>
-.navigation {
-  background-color: rgb(247, 247, 247);
-
-  .version_heart {
-    width: 960px;
-    height: 100px;
-
-    margin: 0 auto;
-
-    display: flex;
-    justify-content: space-evenly;
-
-    .title {
-      line-height: 100px;
-      color: rgb(42, 103, 228);
-
-      font-size: 17px;
-      font-weight: 400;
-
-      &:after {
-        content: '';
-        display: block;
-
-        background-color: rgb(42, 103, 228);
-        width: 100%;
-        height: 4px;
-
-        margin-top: -30px;
-        border-radius: 5px;
-      }
-    }
-  }
-}
-
-.font {
-}
-</style>
